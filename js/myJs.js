75de0a24da0b0e5a4b7acd7a07d1eb8d8e21d68d
thuongdam27 <88798897+thuongdam27@users.noreply.github.com>
2021-08-16 23:54:15 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -133,7 +133,7 @@ $(document).ready(function () {
         Swal.fire({
           width: 900,
           confirmButtonText: textConfig.text12,
-          background: '#fff url("img/iput-bg.jpg")',
+          background: '#fff url("img/input-bg.jpeg")',
           title: textConfig.text10,
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
